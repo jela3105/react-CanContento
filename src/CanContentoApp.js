922688d9641394fea504963b2a123a35069c7020
jela3105 <jela3105@gmail.com>
2021-08-04 19:33:53 -0500

    Create public and private routes components

--- src/CanContentoApp.js
+++ src/CanContentoApp.js
@@ -1,10 +1,6 @@
 import React from "react";
+import { AppRouter } from "./routers/AppRouter";
 
 export const CanContentoApp = () => {
-  return (
-    <>
-      <h1>Hello world</h1>
-      This is Main page
-    </>
-  );
+  return <AppRouter />;
 };
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 export const AppRouter = () => {
-  return <></>;
+  return (
+    <Router>
+      <div>
+        <Switch></Switch>
+      </div>
+    </Router>
+  );
 };
